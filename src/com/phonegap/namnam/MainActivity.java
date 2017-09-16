35582b84b8eac7f2bbd03a1932c6e527bca15323
Maxim Piven <shpax1@gmail.com>
2017-09-16 13:27:32 +0300

    initial play services check

--- src/com/phonegap/namnam/MainActivity.java
+++ src/com/phonegap/namnam/MainActivity.java
@@ -20,10 +20,19 @@
 package com.phonegap.namnam;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
 import org.apache.cordova.*;
 
 public class MainActivity extends CordovaActivity
 {
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "yourtag";
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -35,7 +44,31 @@ public class MainActivity extends CordovaActivity
             moveTaskToBack(true);
         }
 
+        if (playServicesAvailable()) {
+            // ... set up Pusher push notifications
+        } else {
+            // ... log error, or handle gracefully
+        }
+
         // Set by <content src="index.html" /> in config.xml
         loadUrl(launchUrl);
     }
+
+    private boolean playServicesAvailable()
+    {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
 }
